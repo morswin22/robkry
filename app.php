099c4fbe257d59a7815dfb9f737e879317114589
Kacper <kacper@krylowicz.pl>
2018-07-02 12:09:14 +0200

    added login system

--- app.php
+++ app.php
@@ -2,7 +2,29 @@
 
 require_once "lib/router/router.php";
 
+$logged = false;
+if(isset($_SESSION['name'], $_SESSION['pass'])) {
+    $logged = true;
+}
+
+function require_logged() {
+    global $logged;
+    if (!$logged) {
+        error(403);
+    }
+}
+
 route('/', function() {
+    require_logged();
     render('index.html');
-})
+});
+
+route('/login', function() {
+    if (isset($_POST['email'],$_POST['pass'])) {
+        $_SESSION['name'] = $_POST['email'];
+        $_SESSION['pass'] = $_POST['pass'];
+    }
+    redirect('/');
+});
+
 ?>
